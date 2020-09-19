10850d9b379c06a1d62291bd460a3b07f9d2b367
Yuto Watanabe <yuto.w51942@gmail.com>
2020-09-19 23:23:01 +0900

    change @noe

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { NowRequest, NowResponse } from '@vercel/node'
+import { NowRequest, NowResponse } from '@now/node'
 
 export default (request: NowRequest, response: NowResponse) => {
   const { name = 'World' } = request.query
